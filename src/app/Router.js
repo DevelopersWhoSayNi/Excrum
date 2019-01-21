8cdbe88668d5e375a9e7405b89dc86ea469082c4
marianeppinheiro <mahrinoa@gmail.com>
2019-01-21 15:49:35 +0100

    remove log

--- src/app/Router.js
+++ src/app/Router.js
@@ -10,7 +10,6 @@ import UserCreateForm from '../user/userCreate/UserCreateForm';
 import Profile from '../user/userProfile/Profile';
 
 const Routes = props => {
-  console.log(props.Token);
   return (
     <Router>
       <div>
