a18047ac80642e4a6cfc5954a64a270a638b62b7
bayoishola20 <bayoishola20@yahoo.com>
2021-12-02 09:57:36 +0100

    navbar with routes

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Container, Image, Menu } from 'semantic-ui-react';
+import { Container, Image, Menu, Sidebar } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Menu fixed='top'>
       <Container className="App-header">
-        <Link to="/">
-          <Menu.Item as='a' header>
-            <Image size='mini' src={logo} className="App-logo" style={{ marginRight: '0.5em' }} />
-            &lt;Excrum/&gt;
-            </Menu.Item>
-        </Link>
+        <Menu.Item as={Link} to="/" header>
+          <Image size='mini' src={logo} className="App-logo" style={{ marginRight: '0.5em' }} />
+          &lt;Excrum/&gt;
+          </Menu.Item>
+        
+        <Menu.Item as={Link} className="menu-item" activeClassName='active' to="/createTeam">Create Team</Menu.Item>
+        <Menu.Item as={Link} className="menu-item" activeClassName='active' to="/createSprint">Create Sprint</Menu.Item>
+        <Menu.Item as={Link} className="menu-item" activeClassName='active' to="/Sprints">Sprint Overview</Menu.Item>
       </Container>
     </Menu>
   );
